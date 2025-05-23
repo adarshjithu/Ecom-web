7d2e7f823e596ccfe525a01c171d2dde8f604557
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2025-05-23 22:08:44 +0530

    #product

--- src/components/mobile/product/ProductHighlight.jsx
+++ src/components/mobile/product/ProductHighlight.jsx
@@ -1,9 +1,74 @@
-import ProductDescription from "./ProductDescription"
+import ReviewCard from "../ReviewCard";
+import ProductCard from "./ProductCard";
+import ProductDescription from "./ProductDescription";
 
 const ProductHighlight = () => {
+  const reviews = [
+    {
+      name: "Akshay Mohan",
+      verified: true,
+      rating: 5,
+      time: "4 weeks ago",
+      title: "Good product",
+      message:
+        "I received my product so soon and packing is also good. Shampoo is so good. I really loved it.",
+    },
+    {
+      name: "Akshay Mohan",
+      verified: true,
+      rating: 5,
+      time: "4 weeks ago",
+      title: "Good product",
+      message:
+        "I received my product so soon and packing is also good. Shampoo is so good. I really loved it.",
+    },
+  ];
   return (
-    <><ProductDescription/></>
-  )
-}
+    <>
+      <ProductDescription />
+      <div className="px-0 pb-6">
+        <h2 className="text-lg font-semibold mb-4 text-[var(--primary)]">
+          Reviews & Ratings
+        </h2>
+        <div
+          className="flex space-x-2 overflow-x-auto scrollbar-hide"
+          style={{
+            scrollbarWidth: "auto",
+            msOverflowStyle: "auto",
+          }}
+        >
+          {" "}
+          {reviews.map((review, index) => (
+            <div key={index} className="min-w-[300px]">
+              <ReviewCard data={review} />
+            </div>
+          ))}
+        </div>
+      </div>
+        <div className="px-0 pb-6">
+        <h2 className="text-lg font-semibold mb-4 text-[var(--primary)]">
+          Similar Products
+        </h2>
+        <div
+          className="flex space-x-2 overflow-x-auto scrollbar-hide"
+          style={{
+            scrollbarWidth: "auto",
+            msOverflowStyle: "auto",
+          }}
+        >
+          <div className="min-w-[170px]">
+            <ProductCard />
+          </div>
+          <div className="min-w-[170px]">
+            <ProductCard />
+          </div>
+          <div className="min-w-[180px]">
+            <ProductCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ProductHighlight
+export default ProductHighlight;
