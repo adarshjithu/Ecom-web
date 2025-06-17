c9bb90c5e9b87bce3e7a83603bd4f3a77cd55482
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2025-06-17 23:14:02 +0530

    #productCategory

--- src/AppDesktop.jsx
+++ src/AppDesktop.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
+import ScrollToTop from "./ScrollToTop";
 
 function AppDesktop() {
   return (
     <>
       <Header />
+      <ScrollToTop/>
       <Outlet />
       <Footer/>
     </>
