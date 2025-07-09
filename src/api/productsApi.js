9c8877230e353c34d7de2ab519029549ab362015
hello-world-ttj <tijotjoseph003@gmail.com>
2025-07-10 00:39:55 +0530

    #product apis

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -8,3 +8,85 @@ export const getProducts = async (params) => {
     throw error.response.data;
   }
 };
+
+export const getProductsDealOfTheDay = async (params) => {
+  try {
+    const response = await axiosInstance.get(`/products/deal-of-the-day`, {
+      params,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getProductsFeatured = async (params) => {
+  try {
+    const response = await axiosInstance.get(`/products/featured`, { params });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getSimilarProducts = async (id, params) => {
+  try {
+    const response = await axiosInstance.get(`/products/${id}/similar`, {
+      params,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getRecommendedProducts = async (id, params) => {
+  try {
+    const response = await axiosInstance.get(`/products/${id}/recommended`, {
+      params,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getProductsByBrand = async (id, params) => {
+  try {
+    const response = await axiosInstance.get(`/products/brand/${id}`, {
+      params,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getProductsByCategory = async (id, params) => {
+  try {
+    const response = await axiosInstance.get(`/products/category/${id}`, {
+      params,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getProductsBySearch = async (params) => {
+  try {
+    const response = await axiosInstance.get(`/products/search`, { params });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getProduct = async (id) => {
+  try {
+    const response = await axiosInstance.get(`/products/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
